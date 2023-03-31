1b58229f599f010a2af1aa514bc5d87a6fa19932
Kamuri Amorim <27641366+kamuridesu@users.noreply.github.com>
2023-03-30 23:12:52 -0300

    📝 docs(examples/modules/entrypoint.js): update class Entrypoint with new parameters

--- examples/modules/entrypoint.js
+++ examples/modules/entrypoint.js
@@ -1,7 +1,14 @@
+import { commandsHandler } from "./commands.js";
+
+
 class Entrypoint {
     prefix = "!"
     botNumber = "55YYXXXXXXXX";
     ownerNumber = "55YYXXXXXXXX";
+    botName = "ExampleBot";
+    language = "en-us";
+    commandsFilename = "./commands.js";
+ 
     async chatHandlers(bot, message, context, group, metadata) {
         if (message === "test") {
             bot.replyText(context, "YEEEEEEy");
@@ -10,9 +17,7 @@ class Entrypoint {
 
     async commandHandlers(bot, message, context, group, metadata) {
         // In this case, message can be also command to diferentiate the methods.
-        if (message === "test") {
-            bot.replyText(context, "YEEEEEEy");
-        }
+        commandsHandler(bot, message, context, group, metadata);
     }
 }
 
