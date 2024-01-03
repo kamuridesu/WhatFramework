633abb1d7807d0ba451d247d769e8e50b8929d4c
Kamuri Amorim <myk.gata14@gmail.com>
2024-01-03 08:46:08 -0300

    fix: number range from 11-13

--- libs/text.ts
+++ libs/text.ts
@@ -21,7 +21,7 @@ function parseTextWithQuotation(text: string): string[] {
 }
 
 function checkMentionsInText(text: string): string[] {
-	const regex = /@[0-9]{12,13}/g;
+	const regex = /@[0-9]{11,13}/g;
 	const matches = text.match(regex);
 	if (matches) {
 		return matches.map((number) => number.replace("@", "") + "@s.whatsapp.net");
@@ -30,7 +30,7 @@ function checkMentionsInText(text: string): string[] {
 }
 
 function checkJidInTextAndConvert(text: string): { text: string; mentions: string[] } {
-	const regex = /[0-9]{12,13}@s\.whatsapp\.net/;
+	const regex = /[0-9]{11,13}@s\.whatsapp\.net/;
 	while (regex.test(text)) {
 		text = text.replace(regex, "@" + text.match(regex)![0].replace(/@s\.whatsapp\.net/, ""));
 	}
